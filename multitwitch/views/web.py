0850c82aaf839ae6e8d8ff34166c6e0301b037ae
Michael Usher <michael.usher@aarnet.edu.au>
2020-04-13 15:12:01 +1000

    Hack in some scraping to get the channel id

--- multitwitch/views/web.py
+++ multitwitch/views/web.py
@@ -1,5 +1,7 @@
 from multitwitch.lib.session import web, ajax
 from pyramid.response import FileResponse
+from urllib2 import Request, urlopen, URLError
+import re
 
 class WebView:
     @web(template="web/home.tmpl")
@@ -17,3 +19,25 @@ class WebView:
     @staticmethod
     def favicon(request):
         return FileResponse("multitwitch/static/favicon.ico", content_type="image/x-icon")
+
+    @staticmethod
+    def getYTChannelID(request):
+        requrl = 'https://www.youtube.com/' + request.matchdict['username']
+        req = Request(requrl)
+        try:
+            response = urlopen(req)
+        except URLError as e:
+            if hasattr(e, 'reason'):
+                return {'error' : e.reason }
+            if hasattr(e, 'code'):
+                return {'error' : 'Error code: ' + e.code}
+        else:
+            r = response.read()
+            m = re.search('https:\/\/www\.youtube\.com\/channel\/([^\"]*)', r)
+            if not m == None:
+                channel_id = m.group(1)
+                return {'channel_id' : channel_id,
+                        'username' : request.matchdict['username']}
+            else:
+                return {'error' : 'user not found'}
+
